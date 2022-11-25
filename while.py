9ecc230ffa9c8aaf38d4f73b226f3dd92b863dd7
juniornuk <juniornuk318@gmail.com>
2022-11-25 15:56:54 -0300

    commit v1.1

--- while.py
+++ while.py
@@ -3,8 +3,8 @@ import datetime
 #retorno = while_menu.menu()
 retorno = 0
 #while  retorno != 0:
-#ano = input("Informe seu anode nascimento: ")
+ano = int(input("Informe seu ano de nascimento: "))
 
 currentDateTime = datetime.datetime.now()
 date = datetime.date.today()
-print(f"Current Year -> {date.year}")
+print(f"Você têm {date.year - ano} anos")
