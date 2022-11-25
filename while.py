f6e9759ca84d3eccf0717c252b27551af7f94a0e
juniornuk <juniornuk318@gmail.com>
2022-11-25 15:50:58 -0300

    commit v1.0

---
+++ while.py
@@ -0,0 +1,10 @@
+import datetime
+
+#retorno = while_menu.menu()
+retorno = 0
+#while  retorno != 0:
+#ano = input("Informe seu anode nascimento: ")
+
+currentDateTime = datetime.datetime.now()
+date = datetime.date.today()
+print(f"Current Year -> {date.year}")
---
+++ while_menu.py
@@ -0,0 +1,10 @@
+def menu():
+    print("\menu de opções...")
+    print("1 - calcular idade")
+    print("0 - sair do programa")
+    print("")
+
+    print("informe a opção acima: ")
+    opcao = input()
+    return (opcao)
+
