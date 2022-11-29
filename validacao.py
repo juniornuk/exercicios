1e2d0c70881aeacad8972b09247da55ff25d9b5e
juniornuk <juniornuk318@gmail.com>
2022-11-29 16:46:44 -0300

    commit v2.0

--- validacao.py
+++ validacao.py
@@ -16,3 +16,23 @@ def entrada(tipo):
 
 
 
+valor = entrada("nome")
+
+while len(valor) < 3:
+    valor = entrada("nome")
+
+valor = entrada("idade")
+while valor not in range(151):
+    valor = entrada("idade")
+
+valor = entrada("salario")
+while valor < 0:
+    valor = entrada("salario")
+
+valor = entrada("sexo")
+while valor not in ["m", "f"]:
+    valor = entrada("sexo")    e
+
+valor = entrada("ecivil")
+while valor not in ["s", "c", "v", "d"]:
+    valor = entrada("ecivil")    
