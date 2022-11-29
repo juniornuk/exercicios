ed12a546c45d8c87565209919c96e86d695d0467
juniornuk <juniornuk318@gmail.com>
2022-11-29 16:25:42 -0300

    commit v3.0

--- exercicio.py
+++ exercicio.py
@@ -2,3 +2,24 @@ tu = (1, 2, 3, 4, 5)
 a, b, c, d, e = tu
 print(tu[::-1])
 print(type(c))
+
+a, b, c = (2, 4, 6)
+if a > b:
+    if a > c:
+        print(f"{a} é maior")
+    else:
+        print(f"{c} é maior")    
+
+elif b > c:
+    print(f"{b} é maior")
+else:
+    print(f"{c} é maior")    
+
+
+nome = "Osmar"
+valor = ""
+for i in nome:
+    valor = valor + i
+    print(valor)
+
+
--- for.py
+++ for.py
@@ -1,8 +1,12 @@
 for n in range(10):
   print(n)
 
+
+#list compreension
 lista = list((n) for n in range(10)   if (n % 2) != 0)
 print (lista)
 
+
+#funcao lambda
 quadrado = lambda x: x ** 2
 print (quadrado(7))
