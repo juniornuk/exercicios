ee3147403120ed9760ebe7f9d3652515d2e4cfee
juniornuk <juniornuk318@gmail.com>
2022-11-29 17:04:05 -0300

    commit v3.0

--- validacao.py
+++ validacao.py
@@ -1,38 +1,55 @@
 def entrada(tipo):
     if tipo == "nome":
-        return input("Inorme seu nome ")
+        print("nome não pode ser menor que 3 caracteres")
+        return input("Inorme seu nome: ")
 
     if tipo == "idade": 
+        print("Idade deve estar entre 0 e 150")
         return int(input("Informe sua idade: "))
 
     if tipo == "salario":
-        return float(input("Informe seu salario"))
+        print("Valor do salario tem que ser maior que zero")
+        return float(input("Informe seu salario: "))
 
     if tipo == "sexo": 
+        print("Digitar (m) para masculinoe (f) para feminino")
         return input("Informe seu sexo: ")
 
     if tipo == "ecivil":
+        print("digitar (s) para solteiro, (c) para casado, (v) para viuvo e (d) para divorciado")
         return input("Informe seu estado civil: ")
 
 
 
-valor = entrada("nome")
-
+valor = ""
 while len(valor) < 3:
-    valor = entrada("nome")
+    nome = entrada("nome")
+    valor = nome
 
-valor = entrada("idade")
+valor = -1
 while valor not in range(151):
-    valor = entrada("idade")
+    idade = entrada("idade")
+    valor = idade
 
-valor = entrada("salario")
+valor = -1
 while valor < 0:
-    valor = entrada("salario")
+    salario = entrada("salario")
+    valor = salario
 
-valor = entrada("sexo")
+valor = ""
 while valor not in ["m", "f"]:
-    valor = entrada("sexo")    e
+    sexo = entrada("sexo")
+    valor = sexo
 
-valor = entrada("ecivil")
+valor = ""
 while valor not in ["s", "c", "v", "d"]:
-    valor = entrada("ecivil")    
+    ecivil = entrada("ecivil")    
+    valor = ecivil
+
+
+print("\n")
+print(f"nome: {nome}")
+print(f"idade: {idade}")
+print(f"salario: {salario}")
+print(f"sexo: {sexo}")
+print(f"estado civil: {ecivil}")
