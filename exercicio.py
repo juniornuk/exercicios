d36ab55a3eaea70ed9dc163b98e794095a85e3cc
juniornuk <juniornuk318@gmail.com>
2022-11-29 15:38:35 -0300

    commit v2.0

---
+++ exercicio.py
@@ -0,0 +1,4 @@
+tu = (1, 2, 3, 4, 5)
+a, b, c, d, e = tu
+print(tu[::-1])
+print(type(c))
--- for.py
+++ for.py
@@ -1,2 +1,8 @@
-for n in range(100):
+for n in range(10):
   print(n)
+
+lista = list((n) for n in range(10)   if (n % 2) != 0)
+print (lista)
+
+quadrado = lambda x: x ** 2
+print (quadrado(7))
