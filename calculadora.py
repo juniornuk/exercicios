fd74f80c64c6a3c40df891a04dbbcac2165154bf
juniornuk <juniornuk318@gmail.com>
2022-11-29 21:56:10 -0300

    commit v4.0

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,7 @@
+import os
+import time
+
+
 def calcular(val1, val2, operacao):
     match operacao:
         case 1:
@@ -27,25 +31,35 @@ def calcular(val1, val2, operacao):
         
     return resultado, operacao, nome
 
+opcao = ""
+while opcao != "n":
+    print("##### Calculadora #####")
+    print("Escolha uma opção:")
+    print("(1) soma")
+    print("(2) subtração")
+    print("(3) multiplicação")
+    print("(4) divisão")
+    print("(5) potencia")
+    print("(9) fim")
+    print("\n")
+    operando = int(input("opção : "))
 
-print("##### Calculadora #####")
-print("Escolha uma opção:")
-print("(1) soma")
-print("(2) subtração")
-print("(3) multiplicação")
-print("(4) divisão")
-print("(5) potencia")
-print("(9) fim")
-print("\n")
-operando = int(input("opção : "))
-
-if operando == 9:
-    print("Fim")
-    exit(1)
+    if operando == 9:
+        print("Fim")
+        exit(1)
 
 
-val1 = float(input("insira o valor 1 : "))
-val2 = float(input("insira o valor 2 : "))
+    val1 = float(input("insira o valor 1 : "))
+    val2 = float(input("insira o valor 2 : "))
 
-resultado, opcao, nome = calcular(val1, val2, operando)
-print(f"valor de {val1} {opcao} {val2} é = {resultado}")
+    resultado, opcao, nome = calcular(val1, val2, operando)
+    print(f"valor de {val1} {opcao} {val2} é = {resultado}")
+
+    print("\n")
+    opcao = input("Desejafazer outra operacao (s) para sim e (n) para não:")
+    if opcao == "n":
+        print("Fechando calculadora...")
+        time.sleep(3)
+        #limpa = lambda: os.system('clear')
+        #limpa()
+        os.system('clear')
